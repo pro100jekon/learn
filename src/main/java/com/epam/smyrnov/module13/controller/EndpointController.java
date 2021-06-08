05e6106f7c0d00979c4e7a36f1e8d1a85d360144
Yevhenii Smyrnov <Yevhenii_Smyrnov@epam.com>
2021-06-08 18:06:04 +0300

    t

--- src/main/java/com/epam/smyrnov/module13/controller/EndpointController.java
+++ src/main/java/com/epam/smyrnov/module13/controller/EndpointController.java
@@ -1,13 +1,14 @@
 package com.epam.smyrnov.module13.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EndpointController {
 
     @PostMapping("receive")
-    public void receive(String content) {
+    public void receive(@RequestBody String content) {
         System.out.println(content);
     }
 }
