92396af34d6146473d8cc3acdab3974f7c9df0fe
Yevhenii Smyrnov <Yevhenii_Smyrnov@epam.com>
2021-06-09 16:50:07 +0300

    t

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 chmod 777 gradlew
 ./gradlew clean build
+mkdir log
 nohup java -jar logListener/build/libs/logListener-0.0.1-SNAPSHOT.jar > ./log/web.log &
 nohup java -jar orderListener/build/libs/orderListener-0.0.1-SNAPSHOT.jar > ./log/order.log &
